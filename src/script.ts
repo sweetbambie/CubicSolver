8aed881d533df9869322b8c690cc1bd987aa5c89
Sophia Miao <sophia@miao.fm>
2026-03-02 10:38:57 -0500

    added the first case

--- src/script.ts
+++ src/script.ts
@@ -8,6 +8,23 @@ function findRoots () {
   const b = Number(bValue.value);
   const c = Number(cValue.value);
   const d = Number(dValue.value);
-  const p = (3*a*c-b)/(3*a**2)
+  const p = (3*a*c-Math.pow(b, 2))/(3*Math.pow(a, 2));
+  const q = ((27*Math.pow(a, 2)*d - 9*a*b*c + 2*Math.pow(b, 3)))/(27*Math.pow(a, 3))
+
+  const discriminant = Math.pow(q / 2, 2) + Math.pow(p / 3, 3);
+
+  if (discriminant < 0) {
+    const angle = (1/3)*Math.acos(-q/(2*(Math.sqrt(-Math.pow(p/3, 3)))));
+
+    const x1 = (2*(Math.sqrt(-p/3))*Math.cos(angle))-(b/(3*a))
+
+    const x2 = (2*(Math.sqrt(-p/3))*Math.cos(angle +(2*Math.PI)/3))-(b/(3*a))
+    
+    const x3 = (2*(Math.sqrt(-p/3))*Math.cos(angle +(4*Math.PI)/3))-(b/(3*a))
+  } 
+  
+  else {
+  console.log("Use Cardano's Method");
+  } 
 
 }
