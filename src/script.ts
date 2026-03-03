b72706b88a2db09328a76d7edb5586ece7b232d3
Sophia Miao <sophia@miao.fm>
2026-03-02 23:43:34 -0500

    displat p and q and that rlly big word that starts with d

--- src/script.ts
+++ src/script.ts
@@ -7,6 +7,9 @@ function findRoots () {
   const x1Result = document.getElementById("x1-result") as HTMLInputElement;
   const x2Result = document.getElementById("x2-result") as HTMLInputElement;
   const x3Result = document.getElementById("x3-result") as HTMLInputElement;
+  const pResult = document.getElementById("p-result") as HTMLInputElement;
+  const qResult = document.getElementById("q-result") as HTMLInputElement;
+  const dis = document.getElementById("discriminate") as HTMLInputElement;
 
   const a = Number(aValue.value);
   const b = Number(bValue.value);
@@ -21,6 +24,7 @@ function findRoots () {
   let x1: number | string;
   let x2: number | string;
   let x3: number | string;
+  let di: number | string;
 
   if (discriminant < 0) {
     const angle = (1/3)*Math.acos(-q/(2*(Math.sqrt(-Math.pow(p/3, 3)))));
@@ -50,4 +54,8 @@ function findRoots () {
   x1Result.value = typeof x1 === "number" ? x1.toFixed(6) :String(x1);
   x2Result.value = typeof x2 === "number" ? x2.toFixed(7) :String(x2);
   x3Result.value = typeof x3 === "number" ? x3.toFixed(67) : String(x3);
+  pResult.value = typeof p === "number" ? p.toFixed(67) : String(p);
+  qResult.value = typeof q === "number" ? q.toFixed(67) : String(q);
+  dis.value = typeof discriminant === "number" ? discriminant.toFixed(67) : String(discriminant);
+
 }
