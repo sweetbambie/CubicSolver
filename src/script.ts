002a0cbeb3992cf498ac8db00dd9e76237f6b27a
Miao <343417564@tdsb.ca>
2026-03-10 09:06:32 -0400

    smaller grapih

--- src/script.ts
+++ src/script.ts
@@ -60,16 +60,16 @@ document?.addEventListener("submit", (event) => {
   (document.getElementById("q-result") as HTMLInputElement).value = `${q}`;
   (document.getElementById("discriminate") as HTMLInputElement).value = `${discriminant}`;
 
-  ctx.clearRect(0, 0, 600, 600);
+  ctx.clearRect(0, 0, 500, 500);
   ctx.strokeStyle = "gray";
   ctx.lineWidth = 1;
 
-  for (let i = 0; i <= 600; i += 20) {
+  for (let i = 0; i <= 500; i += 20) {
           ctx.beginPath();
           ctx.moveTo(i, 0);
-          ctx.lineTo(i, 600);
+          ctx.lineTo(i, 500);
           ctx.moveTo(0, i);
-          ctx.lineTo(600, i);
+          ctx.lineTo(500, i);
           ctx.stroke();
       }
 
@@ -77,22 +77,22 @@ document?.addEventListener("submit", (event) => {
       ctx.lineWidth = 2;
 
       ctx.beginPath();
-      ctx.moveTo(0, 300);
-      ctx.lineTo(600, 300);
+      ctx.moveTo(0, 250);
+      ctx.lineTo(500, 250);
       ctx.stroke();
 
       ctx.beginPath();
-      ctx.moveTo(300, 0);
-      ctx.lineTo(300, 600);
+      ctx.moveTo(250, 0);
+      ctx.lineTo(250, 500);
       ctx.stroke();
 
       ctx.strokeStyle = "red";
       ctx.lineWidth = 2;
       ctx.beginPath();
-      for (let x = -300; x <= 300; x++) {
+      for (let x = -250; x <= 250; x++) {
           const X = x / 20;
           const y = a * X * X * X + b * X * X + c * X + d;
-          ctx.lineTo(300 + x, 300 - y * 20);
+          ctx.lineTo(250 + x, 250 - y * 20);
           ctx.stroke();
       }
 })
